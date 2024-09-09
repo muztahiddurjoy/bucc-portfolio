e1af26ad9d223e8d24571e4035ac200d7a625607
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-09-09 21:14:41 +0600

    created footer component

--- js/components/footer.js
+++ js/components/footer.js
@@ -5,9 +5,11 @@ class Footer extends HTMLElement {
     }
   
     connectedCallback() {
-        let date = new Date(this.getAttribute('datetime') || Date.now());
+        let year = new Date().getFullYear()
 
-        this.innerHTML = `<h1 style="color:red">Footer nigga</h1>`
+        this.innerHTML = `<div class="flex bg-tertiary" style="display: flex; justify-content: center; padding: 10px;">
+        <p style="font-size: 13px; font-weight: 300">&copy; BUCC ${year} | All Rights Reserved</p>
+     </div>`
     }
   
     disconnectedCallback() {
