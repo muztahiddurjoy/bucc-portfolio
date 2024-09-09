ae09ba50268daee0befe206b91e824efe4a58db5
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-09-10 00:20:08 +0600

    created profiles container

---
+++ js/components/profiles/profiles-container.js
@@ -0,0 +1,50 @@
+class ProfileContainer extends HTMLElement {
+    constructor() {
+      super();
+      console.log("Profile Container Initialized");
+    }
+  
+    connectedCallback() {
+
+        const portfolios = JSON.parse(localStorage.getItem('portfolios')) || [];
+        const user = portfolios.map(portfolio => {
+            return JSON.parse(localStorage.getItem(portfolio));
+        });
+        console.log("USER",user)
+
+        this.innerHTML = `<div class="container full-screen">
+        <h1>Profiles</h1>
+        <div class="flex wrap" style="margin-top:20px">
+        ${user.map(user => `<div class="card" style="font-size: 14px;">
+                <p>Name : <span style="font-weight: 700;">${user.name}</span></p>
+                <p>Age : <span style="font-weight: 700;">${user.age}</span></p>
+                <p>Email : <span class="text-light">${user.email}</span></p>
+                <p>Experiences : <span>${user.experience}</span></p>
+            </div>`).join('')}
+            
+            
+        </div>        
+    </div>s`
+        
+        
+    }
+    
+    
+
+    disconnectedCallback() {
+    }
+  
+    static get observedAttributes() {
+      return [];
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
+    
+    }
+  
+    adoptedCallback() {
+      
+    }
+}
+
+export default customElements.define("profile-container", ProfileContainer);
